53bce69d92488f7e5ee23faca04acdb2618a46b4
chahatmiddha <chahatmiddha@gmail.com>
2022-09-30 20:03:21 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ArticleList from "./articleList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <ArticleList />
+    </div>
+  );
+}
